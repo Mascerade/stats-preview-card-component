489a63279a14d45ea2ed47e4c20f85df73ae8d02
Jason Acheampong <Ultimi450@gmail.com>
2021-11-05 00:24:05 -0400

    got the desktop version working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './public/**/*.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ['Inter'],
+        LexendDeca: ['Lexend Deca'],
+      }
+    },
+    colors: {
+      darkblue: '#090b1a',
+      desatblue: '#1b1938',
+      violet: '#aa5cdb',
+      white: '#ffffff', 
+    }
   },
   variants: {
     extend: {},
